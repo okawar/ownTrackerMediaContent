c4f3c2d9b3227670e19307aee70cf68e5402b027
Oka <lehaloot@mail.ru>
2025-11-24 06:35:01 +0300

    Created Canvas

--- src/components/MapCanvas.vue
+++ src/components/MapCanvas.vue
@@ -1,11 +1,51 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue';
+import { onMounted } from 'vue';
+import { useMapGenerator } from '../composables/useMapGenerator';
 
+const { heightMap, generateMap } = useMapGenerator();
+const canvasEl = ref<HTMLCanvasElement | null>(null);
 
+onMounted(() => {
+    generateMap(512, 512)
+})
 
+watch(heightMap, (mapData) => {
+
+    const canvas = canvasEl.value;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+
+    if (!mapData || !mapData.length || mapData[0] === undefined) return;
+
+    const width = mapData.length;
+    const height = mapData[0].length;
+
+    canvas.width = width;
+    canvas.height = height;
+
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            const row = mapData[x];
+            if (!row) continue;
+
+            const cellValue = row[y];
+            if (cellValue === undefined) continue;
+
+            const value = (cellValue + 1) / 2 * 255; 
+
+            const color = Math.floor(value);
+            ctx.fillStyle = `rgb(${color}, ${color}, ${color})`;
+            ctx.fillRect(x, y, 1, 1);
+        }
+    }
+})
 </script>
 
 <template>
 
-<h1>Map Canvas</h1>
+    <canvas id="mapCanvas" ref="canvasEl"></canvas>
 
 </template>
--- src/composables/useMapGenerator.ts
+++ src/composables/useMapGenerator.ts
@@ -1,3 +1,4 @@
+import { createNoise2D } from "simplex-noise";
 import { ref } from "vue";
 
 
@@ -6,7 +7,20 @@ export function useMapGenerator() {
     const heightMap = ref<number[][]>([])
 
     function generateMap(width: number, height: number) {
-        
+        const noise2D = createNoise2D()
+
+        const data: number[][] = []
+        const scale = 50
+
+        for (let x = 0; x < width; x++) {
+            const row: number[] = []
+            for (let y = 0; y < height; y++) {
+                const value = noise2D(x / scale, y / scale)
+                row.push(value)
+            }
+            data.push(row)
+        }
+        heightMap.value = data
     }
     
     return {
